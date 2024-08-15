de4b5420af06c844c0d239da855e623bdbfb6fad
rajnish <rajnishkumar550443@gmail.com>
2024-08-15 22:41:58 +0530

    add resume

--- js.js
+++ js.js
@@ -1,3 +1,19 @@
+//send data to mail
+function sentEmail() {
+    Email.send({
+        SecureToken: "e426ef0b-94f8-44f7-8010-8010184eef51",
+        To: 'rajnishkumar550443@gmail.com',
+        From: "rajnishkumar550443@gmail.com",
+        Subject: "This is the subject",
+        Body: "Name: " + document.getElementById("name").value
+             +"<br> Email: " + document.getElementById("email").value
+             +"<br> Phone: " + document.getElementById("phone").value
+             +"<br> Message: " + document.getElementById("message").value
+    }).then(
+         message => swal("Success", "We will contact you soon.")
+    );
+};
+
 // toggle icon navbar
 let menuIcon = document.querySelector('#menu-icon')
 let navbar = document.querySelector('.navbar')
