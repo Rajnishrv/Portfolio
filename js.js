39573332b86406ec203695ee6f838e8f0d3b7555
rajnish <rajnishkumar550443@gmail.com>
2024-08-16 19:39:39 +0530

    fixed bugs

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-//send data to mail
+//send data to email
 function sentEmail() {
     Email.send({
         SecureToken: "e426ef0b-94f8-44f7-8010-8010184eef51",
@@ -6,11 +6,11 @@ function sentEmail() {
         From: "rajnishkumar550443@gmail.com",
         Subject: document.getElementById("subject").value,
         Body: "Name: " + document.getElementById("name").value
-             +"<br> Email: " + document.getElementById("email").value
-             +"<br> Phone: " + document.getElementById("phone").value
-             +"<br> Message: " + document.getElementById("message").value
+            + "<br> Email: " + document.getElementById("email").value
+            + "<br> Phone: " + document.getElementById("phone").value
+            + "<br> Message: " + document.getElementById("message").value
     }).then(
-         message => swal("Success", "We will contact you soon.")
+        message => swal("Success", "We will contact you soon.")
     );
 };
 
@@ -18,7 +18,7 @@ function sentEmail() {
 let menuIcon = document.querySelector('#menu-icon')
 let navbar = document.querySelector('.navbar')
 
-menuIcon.onclick = ()=>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active')
 };
@@ -27,35 +27,35 @@ menuIcon.onclick = ()=>{
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = ()=>{
-    section.forEach(sec =>{
+window.onscroll = () => {
+    section.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links =>{
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nev a[href*='+ id +']').classList.add('active');
+                document.querySelector('header nev a[href*=' + id + ']').classList.add('active');
             });
         };
-    }); 
-// sticky navbar
-let header = document.querySelector('header');
-header.classList.toggle('sticky', window.scrollY > 100);
+    });
+    // sticky navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-// remove toggle icon and navbar when click navbar link scroll
-menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active');
+    // remove toggle icon and navbar when click navbar link scroll
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 // scrall reveal
-ScrollReveal({ 
-    reset: true, 
+ScrollReveal({
+    reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200,
-    
+
 });
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
@@ -63,9 +63,8 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // type js
-
-const typed = new Typed('.multiple-text',{
-    strings:['Frontend Developer', 'Backend Developer', 'Fullstack Developer'],
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer', 'Fullstack Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
@@ -73,6 +72,6 @@ const typed = new Typed('.multiple-text',{
 })
 
 let aboutBtn = document.getElementById('sklbtn');
-aboutBtn.onclick =()=>{
+aboutBtn.onclick = () => {
     aboutBtn.classList.toggle("rajnish kumar")
 }
